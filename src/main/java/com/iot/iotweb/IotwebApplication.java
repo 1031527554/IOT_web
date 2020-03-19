e4371d7ff853f59fa322f4a93ad61cc474aae792
ls103 <1031527554@qq.com>
2020-03-20 01:16:31 +0800

    first

---
+++ src/main/java/com/iot/iotweb/IotwebApplication.java
@@ -0,0 +1,13 @@
+package com.iot.iotweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iot/iotweb/IotwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.iotweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
